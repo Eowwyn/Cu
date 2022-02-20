a65cf68e2d54d307df59d081fde871a802b17a6b
Andrey <87265048+Eowwyn@users.noreply.github.com>
2022-02-20 13:47:04 +0700

    Update P2.Ex2.c

--- P2.Ex2.c
+++ P2.Ex2.c
@@ -1,1 +1,26 @@
+#include <math.h>
 
+
+int main(){
+
+    int n, i=1, k=1;
+    float x;
+    double c;
+
+
+    printf("Enter n\n");
+    scanf("%d",&n);
+    printf("Enter x\n");
+    scanf("%f", &x);
+
+
+    while(i<=n){
+
+        k = k * i;
+        c = c + pow(-1,i) * pow(x,2*i)/2*k;
+        ++i;
+    }
+
+    printf("%lf", c);
+    return 0;
+}
