29c907544284f22504da7b3fac595d3c6eae334b
Andrey <87265048+Eowwyn@users.noreply.github.com>
2022-02-23 15:52:11 +0700

    Update P2.Ex2.c

--- P2.Ex2.c
+++ P2.Ex2.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
-int main()
-{
-    double x;//сам х
-    double cos = 1;//общая сумма
-    double down = 1;//знаменатель(факториал)
-    double up = x*x;//числитель (увеличение степени х на 2)
-    int n ; // точность
-
-    printf("Enter E: \n");
-    scanf("%d", n);
+int main(){
     
-    int i;
-    printf("Enter x: ");
+    int n,s,i,j;
+    double cos=0, x;
+    
+    printf("Please enter x: ");
     scanf("%lf", &x);
-    for (i = 2; i < n; i+=2)//сразу с 2 и по 2 в будущем
-    {
-        up *= (-1)*up;//контроль -1 и увеличение степени х
-        down *= (i-1) * i; //факториал
-        cos += up / down; //добавление следующего компонента
+    printf("Please enter n: ");
+    scanf("%d", &n);
+    
+    for(i=0;i<=n;i++){
+        
+     for(j=0;j<=2*i;j++){
+         if(j==0){
+            s=1; 
+         } else {
+             s*=j;
+         }
+     }   
+     cos+=pow(-1,i)*pow(x,2*i)/s ;   
+        
     }
-    printf("result: %lf\n", x);
-
+    printf("cos(x)=%lf", cos);
     return 0;
 }
