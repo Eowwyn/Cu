8e62ddb1d5359b644a414c39e4d6bbc7b7519f7b
Andrey <87265048+Eowwyn@users.noreply.github.com>
2022-02-28 21:23:26 +0700

    Update P2.Ex1.c

--- P2.Ex1.c
+++ P2.Ex1.c
@@ -1,1 +1,38 @@
-13
+// Обменять местами максимальный и минимальный элементы массива. Вывести измененный массив.
+
+#include <stdio.h>
+
+int main(){
+    
+    int N; 
+    printf("Enter N: ");
+    scanf("%d", &N);
+    
+    int b,s,k;
+    int arr[N];
+    int max = arr[0];
+
+    for(int i=0;i<N;i++){
+        printf("Enter arr[i]: ");
+        scanf("%d", &arr[i]);
+        
+        if(arr[i]>max){
+            max = arr[i];
+            b = i;
+        }
+    }
+    int min = arr[0];
+     for(int i=0;i<N;i++){
+         if(arr[i]<min){
+            min = arr[i];
+            s = i;
+        }
+     }
+     k = arr[s];
+     arr[s] = arr[b];
+     arr[b] = k;
+     for(int i=0;i<N;i++){
+         printf("%d", arr[i]);
+        }
+    return 0;
+}
