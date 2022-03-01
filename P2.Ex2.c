3d6d66ac0e8add2ce195a5ea8fba5d278bb8e788
Andrey <87265048+Eowwyn@users.noreply.github.com>
2022-03-01 21:47:58 +0700

    Update P2.Ex2.c

--- P2.Ex2.c
+++ P2.Ex2.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main(){
+    
+  int N,s=0;
+  printf("Enter N: ");
+  scanf("%d", &N);
+  
+  int arr[N];  
+  
+  for(int i=0;i<N;i++){
+    printf("Enter arr[i]: ");
+    scanf("%d", &arr[i]); 
+  } 
+  
+  for(int i=0;i<N-1;i++){
+      if(arr[i]>arr[i+1]){
+        s+=1; 
+      } else {
+        s+=0;
+      }
+  }
+  printf("%d \n", s);
+  if(s==0){
+    printf("Yes");  
+  } else {
+    printf("No");
+  }
+  return 0;
+}
