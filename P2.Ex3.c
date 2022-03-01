54b6c9399cb2f8a8e956fb27ed0bfacab0cc78a8
Andrey <87265048+Eowwyn@users.noreply.github.com>
2022-03-01 22:56:39 +0700

    Create P2.Ex3.c

---
+++ P2.Ex3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    
+ int N;
+ int s=0,k;
+
+ int arr[N];
+ printf("Enter N: ");
+ scanf("%d", &N);   
+    
+ for(int i=0;i<N;i++){
+    printf("Enter arr[i]: ");
+    scanf("%d", &arr[i]);
+ }    
+   for(int i=0;i<N;i++){
+      s+=arr[i]; 
+      k=s;
+   } 
+   int c=0;
+   for(int i=0;i<N;i++){
+       if(arr[i]<(k/N)){
+        c+=1;
+       } 
+   }
+   printf("K: %d \n", k/N); //  среднее арифметическое
+   printf("C: %d", c); // с - количество элементов < ср.арифм.
+   
+   return 0;
+}
