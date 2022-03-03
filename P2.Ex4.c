cca13313f39621e1acb6b759707057e444084435
Andrey <87265048+Eowwyn@users.noreply.github.com>
2022-03-03 22:23:37 +0700

    Update P2.Ex4.c

--- P2.Ex4.c
+++ P2.Ex4.c
@@ -20,3 +20,38 @@ int main(){
 // либо через 2 счетчика
 
 // доделать через два счетчика
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b; 
+    *b = temp;
+}
+
+int main(){
+    
+ int N;
+
+  printf("Enter N: ");
+  scanf("%d", &N);   
+  
+  int arr[N];
+  for(int i=0;i<N;i++){
+     printf("Enter arr[i]: ");
+     scanf("%d", &arr[i]);  
+  }
+  
+  if(N%2==0){
+    for(int i=0,j=N-1 ; i<=N/2 && j >= N/2; i++, j-- )
+     
+    swap(&arr[i],&arr[j]); 
+  
+  } else if(N%2!=0){
+    for(int i=0,j=N-1 ; i<=N/2 && j >= N/2; i++, j-- )  
+      
+      
+  }
+  
+   return 0;
+}
