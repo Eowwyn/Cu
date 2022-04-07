0ab06788b4e06be0d877d3a6804583ea5c49a7d2
Andrey <87265048+Eowwyn@users.noreply.github.com>
2022-04-07 14:20:22 +0700

    Update 1. 2.3.4.c

--- 1. 2.3.4.c
+++ 1. 2.3.4.c
@@ -1,13 +1,13 @@
-#include stdio.h
-#include ctype.h
-#include string.h
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #define N 1024
 
- //Schitaem glasnie bukovki
+// Schitaem glasnie bukovki
 int glasnie_bukovki(char t){
-     char glas[] = eEyYuUiIoOaA;
+     char glas[] = "eEyYuUiIoOaA";
      int L = strlen(glas);
-         for(int i = 0; i  L; i++){ 
+         for(int i = 0; i < L; i++){ 
              if(t == glas[i]){
                  return 1;
              } else {
@@ -17,42 +17,45 @@ int glasnie_bukovki(char t){
 }
 
 
- //Proverka + print
-void proverka_printf(char string){
-     if (string == NULL) obnulenie pokazatelya
+// Proverka + print
+void proverka_printf(char *string){
+     if (string == NULL) //obnulenie pokazatelya
             return;
         int gl = 0, sgl = 0;  
-        int k = 0, b = 0;
-        for(int p = 0; string[i] != '0'; p++){ until not end
-            for (int j = k; string[j] != '0' && !ispunct(string[j]);j++) { ; } 
-            for (int i = k; ij; i++) { schitaem bukovki
+        int k = 0, p, f = 0;
+        
+        for(int p = 0; string[p] != '\0'; p++){ //until not end or mb while 
+            for (int j = k; string[j] != '\0' && !ispunct(string[j]);j++) { 
+                 ++f;
+            } 
+            for (int i = k; i < f; i++) { //schitaem bukovki
                 if(glasnie_bukovki(string[i])==1){
                     ++gl;
                 } else {
                     ++sgl;
                 }
             }
-            if((gl != 0  sgl != 0) && (gl  sgl)){
-               for(int t = k; t  j; t++){
-                   printf(%c, string[t]);
+            if((gl != 0 || sgl != 0) && (gl < sgl)){
+               for(int t = k; t < f; t++){
+                   printf("%c", string[t]);
                }
             }
-            while(ispunct(string[j+1])){
-                ++j;
+            while(ispunct(string[f + 1])){ // change
+                ++f;
             }
-            k = j + 1;
+            gl = 0;
+            sgl = 0;
+            k = f + 1;
         }
-        
 }  
 
 int main() {
   
   char strochka[N];
-  printf(Bruh, enter );
+  printf("Bruh, enter: ");
   gets(strochka);
   
   proverka_printf(strochka);
-  
   
     return 0;
 }
